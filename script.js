328ecdcc40ffa5dc594ae8cbe63a394d0eab2331
senyka33 <47477921+senyka33@users.noreply.github.com>
2022-01-21 19:47:27 +0300

    feat: lazy scroll

--- script.js
+++ script.js
@@ -66,3 +66,27 @@ function retrieveFormValue(event) {
 
 form.addEventListener('submit', retrieveFormValue);
 
+// Scroll
+const links = document.querySelectorAll('.topnav a')
+
+links.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault()
+
+        const id = element.getAttribute('href').substring(1)
+        const section = document.getElementById(id)
+
+        if (section) {
+            seamless.elementScrollIntoView(section, {
+                behavior: 'smooth',
+                block: 'start'
+            })
+        } else {
+            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    })
+})
